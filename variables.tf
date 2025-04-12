e8098d112d58f8fa841cc0b182326d87a04817c0
abhishekarora099 <abhishekarora099@yahoo.in>
2025-04-13 01:23:38 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "ami" {
+    type = string
+    default = "ami-0eee20974fdb103cc"
+}
+
+variable "az" {
+    type = string
+    default = "ap-south-1a"
+}
+
+variable "dbsize" {
+    type = number
+    default = 10
+}
+
+variable "dbport" {
+    type = string
+    default = "3306"
+}
+
+variable "typeofinstance" {
+    type = string
+    default = "t2.micro"
+}
+
+variable "keyname" {
+    type = string
+    default = "TerraformKeyPair2"
+}
